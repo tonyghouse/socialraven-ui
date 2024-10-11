dcb77eb55ab4058a67bea66ea9a074ccfc46c0f4
tonyghouse <tonyghouseofficial@gmail.com>
2024-10-11 10:08:13 +0530

    improved scheduled posts UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,13 +12,6 @@ function Navbar() {
       <span className="ml-2 text-lg font-bold">Social Raven</span>
     </Link>
     <nav className="ml-auto flex gap-4 sm:gap-6">
-      <Link className="text-sm font-medium hover:text-orange-500 transition-colors" href="/">
-        Features
-      </Link>
-
-      <Link className="text-sm font-medium hover:text-orange-500 transition-colors" href="/">
-        About
-      </Link>
     </nav>
     {
     (userId) ?  <div className="ml-4 flex gap-2"> <UserButton /> </div>:
