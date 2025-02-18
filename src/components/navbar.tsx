4c9f41d89415fee428fca251f1f145524d5bfb81
Tony Ghouse <tonyghouseofficial@gmail.com>
2025-02-18 23:04:38 +0530

    clerk js client side issue fixed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,15 @@
 'use client'
   import Link from 'next/link'
   import React from 'react'
-  import { UserButton } from "@clerk/nextjs"
-  import { auth } from '@clerk/nextjs/server'
+  import { UserButton, useUser } from "@clerk/nextjs"
+
   import { Button } from './ui/button'
+
   
 export default function Navbar() {
-      const { userId }: { userId: string | null } = auth()
+      // const { userId }: { userId: string | null } = auth()
+      const { isSignedIn, user } = useUser();
+
     return (
       <header className="px-4 lg:px-6 h-14 flex items-center">
       <Link className="flex items-end justify-center" href="/">
@@ -15,7 +18,7 @@ export default function Navbar() {
       <nav className="ml-auto flex gap-4 sm:gap-6">
       </nav>
       {
-      (userId) ?  <div className="ml-4 flex gap-2"> <UserButton /> </div>:
+      (isSignedIn) ?  <div className="ml-4 flex gap-2"> <UserButton /> </div>:
       <div className="ml-4 flex gap-2">
         <Link href={"/sign-in"}>
         <Button variant="outline">Sign In</Button>
