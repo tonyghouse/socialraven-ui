76611bc44d789fd3cb7a5e2448d8c5c721d88f6c
Tony Ghouse <tonyghouseofficial@gmail.com>
2025-02-18 10:30:25 +0530

    nav  bar added back

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,31 @@
 'use client'
-
-export default function Navbar() {
+  import Link from 'next/link'
+  import React from 'react'
+  import { UserButton } from "@clerk/nextjs"
+  import { auth } from '@clerk/nextjs/server'
+  import { Button } from './ui/button'
   
-
-  return (
-    <div className="max-w-6xl mx-auto">
-      Dummy Navbar
-    </div>
-  )
-}
+export default function Navbar() {
+      const { userId }: { userId: string | null } = auth()
+    return (
+      <header className="px-4 lg:px-6 h-14 flex items-center">
+      <Link className="flex items-end justify-center" href="/">
+        <div className="ml-1 text-base font-bold">SocialRaven</div>
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+      </nav>
+      {
+      (userId) ?  <div className="ml-4 flex gap-2"> <UserButton /> </div>:
+      <div className="ml-4 flex gap-2">
+        <Link href={"/sign-in"}>
+        <Button variant="outline">Sign In</Button>
+        </Link>
+        
+        <Link href={"/sign-up"}>
+        <Button variant="secondary">Sign Up</Button>
+        </Link>
+      </div>
+     }
+    </header>
+    )
+  }
